7be673fd33ecf584c702f6c4764d16f357c55707
elderfieldzeus <elderfieldzeus24@gmail.com>
2024-08-10 01:18:48 +0800

    improved bottom padding on main

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -8,10 +8,10 @@ interface MainProps {
 
 const Main: React.FC<MainProps> = ({children, header}) => {
   return (
-    <div className='w-full min-h-screen bg-white relative pb-20'>
+    <div className='w-full min-h-screen bg-white relative'>
         <Navbar />
         <p className='font-coffee flex items-center text-2xl fixed top-0 h-14 px-5 w-full bg-white'>{header}</p>
-        <main className='pt-16 px-5'>
+        <main className='py-16 px-5'>
             {children}
         </main>
     </div>
