add21a9bdb0de30186cee22f45143e73d978185e
elderfieldzeus <elderfieldzeus24@gmail.com>
2024-08-09 16:26:34 +0800

    changed padding for header:

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -10,7 +10,7 @@ const Main: React.FC<MainProps> = ({children, header}) => {
   return (
     <div className='w-full min-h-screen bg-white'>
         <Navbar />
-        <main className='pt-10 px-5'>
+        <main className='pt-2 px-5'>
             <p className='font-coffee text-2xl'>{header}</p>
             {children}
         </main>
