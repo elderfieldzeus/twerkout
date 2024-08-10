2a7d2704e5ab4f56ebf2ab6179b5d029dfd984d7
elderfieldzeus <elderfieldzeus24@gmail.com>
2024-08-10 17:16:38 +0800

    added profile display

--- src/components/Profile/InfoSection.tsx
+++ src/components/Profile/InfoSection.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Info from './Info'
+import { User } from 'firebase/auth'
 
-const InfoSection: React.FC = () => {
+interface InfoSectionProps {
+  user: User | null;
+}
+
+const InfoSection: React.FC<InfoSectionProps> = ({user}) => {
+  const [date, setDate] = useState<string | null>(null);
+
+  useEffect(() => {
+    if(user && user.metadata.creationTime) {
+      const tempDate = new Date(user.metadata.creationTime).toLocaleDateString();
+      setDate(tempDate);
+    }
+  }, [user]);
+
   return (
     <div className='flex flex-col items-center'>
-        <Info header="email" data="rooster@gmail.com" />
-        <Info header="date joined" data="08-09-2024" />
+        <Info header="email" data={user && user.email ? user.email : ''} />
+        <Info header="date joined" data={user && user.metadata.creationTime ? date : ''} />
         <Info header="no. of workouts" data="0" />
     </div>
   )
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,14 +1,26 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Main from '../components/Main';
 import ProfilePicture from '../components/Profile/ProfilePicture';
 import InfoSection from '../components/Profile/InfoSection';
 import SignoutSection from '../components/Profile/SignoutSection';
+import { auth } from '../utilities/firebase';
+import { User } from 'firebase/auth';
 
 interface ProfileProps {
     setColor: () => void;
 }
 
 const Profile: React.FC<ProfileProps> = ({setColor}) => {
+    const [user, setUser] = useState<User | null>(null);
+
+    useEffect(() => {
+        setColor();
+    }, [setColor]);
+
+    useEffect(() => {
+      setUser(auth.currentUser);
+    }, []);
+
     useEffect(() => {
         setColor();
     }, [setColor])
@@ -16,7 +28,7 @@ const Profile: React.FC<ProfileProps> = ({setColor}) => {
   return (
     <Main header='Profile'>
         <ProfilePicture />
-        <InfoSection />
+        <InfoSection user = {user}/>
         <SignoutSection />
     </Main>
   )
