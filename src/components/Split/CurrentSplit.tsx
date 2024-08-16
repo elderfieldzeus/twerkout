c1b2e7cccaa091d0f28bd9a9894b1854dc433383
elderfieldzeus <elderfieldzeus24@gmail.com>
2024-08-16 17:34:57 +0800

    feat: added bg color to current split

--- src/components/Split/CurrentSplit.tsx
+++ src/components/Split/CurrentSplit.tsx
@@ -4,12 +4,16 @@ interface CurrentSplit {}
 
 const CurrentSplit: React.FC<CurrentSplit> = () => {
   return (
-    <button className="flex h-44 w-full items-start rounded-xl border-2 border-black bg-white px-4 font-coffee shadow-xl active:bg-gray-100 justify-between">
+    <button className="flex h-44 w-full items-start justify-between rounded-xl border-2 border-black bg-yellow-400 px-4 font-coffee shadow-xl transition-all active:bg-yellow-500">
       <div className="pt-6 text-start">
         <p>Your current split:</p>
-        <p className="text-gray-500">N/A</p>
+        <p className="text-gray-600">N/A</p>
       </div>
-      <img className="h-full select-none" src="/images/chickenlift.png" alt="" />
+      <img
+        className="h-full select-none"
+        src="/images/chickenlift.png"
+        alt=""
+      />
     </button>
   );
 };
